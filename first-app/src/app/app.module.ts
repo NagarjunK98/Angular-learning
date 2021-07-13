5c78d6ac2f86e537d145fa48ddeb72c48589ad69
Nagarjun k <53399371+nagarjunkumar@users.noreply.github.com>
2021-07-13 13:24:45 +0530

    event binding and two way binding concepts

--- first-app/src/app/app.module.ts
+++ first-app/src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { FruitComponent } from './fruits/fruits.component';
 @NgModule({
   declarations: [AppComponent, FruitComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
